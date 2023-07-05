1dce45e5cdf365f6799e8d309e04909e7ef792dc
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-07-05 17:38:06 +0200

    Player movement ready

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CustomInput input = null;
+    private Vector2 moveVector = Vector2.zero;
+    private Rigidbody2D rb;
+    private float moveSpeed = 10f;
+
+    private void Awake()
     {
-        
+        input = new CustomInput();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPerformed;
+        input.Player.Movement.canceled += OnMovementStopped;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPerformed;
+        input.Player.Movement.canceled -= OnMovementStopped;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log(moveVector);
+        rb.velocity = moveVector * moveSpeed;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementStopped(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
     }
 }
