4c9bf0595d78a9348cfc2081124cf93b236bba3c
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2023-07-18 12:25:01 +0200

    Added batman

--- Assets/Scripts/Enemies/Bat.cs
+++ Assets/Scripts/Enemies/Bat.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Bat : Enemy
 {
     // Start is called before the first frame update
-    void Start()
+      protected override void Attack ()
     {
         base.Attack();
     }
 
     // Update is called once per frame
-    void Update()
+   protected override void Animate()
     {
         base.Animate();
     }
