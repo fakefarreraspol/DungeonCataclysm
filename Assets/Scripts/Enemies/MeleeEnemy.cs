6c456dc3067405bb6a39556b9e69314ee1b09a64
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-07-27 17:00:22 +0200

    melee attack ready

--- Assets/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class MeleeEnemy : Enemy
 {
+    protected Transform attackingPoint;
+    [SerializeField] protected float attackRange;
+    [SerializeField] protected Vector2 APOffset;
+
+    [SerializeField] private LayerMask enemyLayers;
+
+    protected override void Start()
+    {
+        attackingPoint = gameObject.transform.Find("AttackingPoint");
+        attackingPoint.transform.position = new Vector2(transform.position.x, transform.position.y) + APOffset;
+        base.Start();
+    }
     protected override void Decide()
     {
         if (Vector2.Distance(transform.position, enemyTarget.position) < chaseDistance)
@@ -15,16 +27,34 @@ public class MeleeEnemy : Enemy
             }
             else Attack();
         }
+
+        flipAttackingPoint();
     }
 
     protected override void Attack()
     {
-        Debug.Log("Attacked");        
         base.Attack();
     }
 
     protected virtual void DoDamage(int damage)
     {
         playerCharacter.ReceiveDamage(damage);
+    }
+
+    protected void IsPlayerHit()
+    {
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackingPoint.position, attackRange, enemyLayers);
+        
+        foreach(Collider2D enemy in hitPlayer)
+        {
+            Debug.Log("PlayerIsHit");
+            DoDamage(enDamage);
+        }
+    }
+
+    protected virtual void flipAttackingPoint()
+    {
+        if (enSprRenderer.flipX) attackingPoint.transform.position = (Vector2)transform.position + new Vector2 (-APOffset.x, -APOffset.y);
+        else attackingPoint.transform.position = (Vector2)transform.position + new Vector2 (APOffset.x, -APOffset.y);
     }
 }
